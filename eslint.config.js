06110c7a60e912961c2fc295c60b523e7f2a9b83
Nurul Imam <me@nurulimam.com>
2025-07-26 12:34:09 +0700

    Update Lint & Pretier

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+// eslint.config.js
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import svelte from "eslint-plugin-svelte";
+
+export default [
+  ...js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    files: ["**/*.ts", "**/*.js"],
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        extraFileExtensions: [".svelte"],
+      },
+    },
+    plugins: {
+      svelte: svelte,
+    },
+    processor: svelte.processors.svelte,
+  },
+];
